88cb30b73978eecf61cddbb3b4fcd1672dc1b2b1
Shrey Chauhan <shreychauhan0573@gmail.com>
2020-03-02 10:36:38 +0530

    Updates

---
+++ precache-manifest.60f472238dc87b7b5a8b3bc860af4c73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73e1e2a2b613b40f3958201c7aa9247",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "b0e556ad5a390ab95e5f",
+    "url": "/admin-dashboard/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "be67e3065678531aa71a",
+    "url": "/admin-dashboard/static/css/main.0b0fe01a.chunk.css"
+  },
+  {
+    "revision": "b0e556ad5a390ab95e5f",
+    "url": "/admin-dashboard/static/js/2.ca8f2041.chunk.js"
+  },
+  {
+    "revision": "bc82a7a92f697e60ec51f64853cfb7ed",
+    "url": "/admin-dashboard/static/js/2.ca8f2041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be67e3065678531aa71a",
+    "url": "/admin-dashboard/static/js/main.faebe3e3.chunk.js"
+  },
+  {
+    "revision": "9d5c0b08dda147f60b7c",
+    "url": "/admin-dashboard/static/js/runtime-main.a664ceda.js"
+  }
+]);
